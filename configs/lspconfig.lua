85f55856522e07d0cf09c3d5848239f51bae322d
Sharn-Konet Reitsma <sharnkonet.reitsma@gmail.com>
2023-03-29 22:31:53 +1300

    rust-analyzer has no inbuilt config, make your own

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "terraformls", "julials", "marksman", "yamlls", "taplo", "svelte", "kotlin_language_server", "ltex", "dockerls", "docker_compose_language_service", "bashls", "rust-analyzer" }
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "terraformls", "julials", "marksman", "yamlls", "taplo", "svelte", "kotlin_language_server", "ltex", "dockerls", "docker_compose_language_service", "bashls", }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -12,6 +12,20 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.rust_analyzer.setup {
+  settings = {
+    ["rust_analyzer"] = {
+      workspace = {
+        symbol = {
+          search = {
+            kind = "all_symbols"
+          }
+        }
+      }
+    }
+  }
+}
 
 -- 
 -- lspconfig.pyright.setup { blabla}
